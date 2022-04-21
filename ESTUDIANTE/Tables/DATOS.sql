b96094169ad722d59e4ab3d8f7a9c759e586f220
Niyi08 <64096713+Niyi08@users.noreply.github.com>
2022-04-21 10:03:10 -0500

    Update DATOS.sql
    21 bits

--- ESTUDIANTE/Tables/DATOS.sql
+++ ESTUDIANTE/Tables/DATOS.sql
@@ -1,7 +1,7 @@
 CREATE TABLE estudiante.datos (
   direccion VARCHAR2(20 BYTE),
   identificacion NUMBER NOT NULL,
-  curso VARCHAR2(20 BYTE),
+  curso VARCHAR2(21 BYTE),
   clase VARCHAR2(20 BYTE),
   CONSTRAINT datos_pk PRIMARY KEY (identificacion)
 );
